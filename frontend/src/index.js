01ec5e63b7b870febfff68032fe021e002f71ad9
BigHAAS <njrosenstiel@gmail.com>
2021-05-07 09:42:31 -0500

    added user authentication and local storage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,40 +1,28 @@
-import { render } from '@testing-library/react';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import Login from "./login-screen";
 import HomeScreen from "./homeScreen";
-
+import useToken from './useToken';
 
 import {
     BrowserRouter as Router, 
     Redirect, 
     Route,
-    Switch,
 } from "react-router-dom";
 
 function App(){
-    const [token, setToken] = useState();
+    const { token, setToken } = useToken();
+    console.log(token);
     if(!token){
-        return (
-            <Router>
-                <Redirect exact from="/" to="/login" />
-                <Route path="/login">
-                    <Login/>
-                </Route>
-            </Router>
-        );
+        return <Login setToken={setToken}/>
     }
     return (
         <Router>
-            <div>
-                <Switch>
-                    <Redirect exact from="/" to="/home"/> 
-                    <Route path="/home">
-                        <HomeScreen/>
-                    </Route>
-                </Switch>
-            </div>
+            <Redirect exact from="/" to="/home"/> 
+            <Route path="/home">
+                <HomeScreen/>
+            </Route>
         </Router>
     );
 }
--- frontend/src/login-screen.js
+++ frontend/src/login-screen.js
@@ -1,30 +1,14 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-import HomeScreen from "./homeScreen";
+export default function Login( { setToken } ){
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
 
-import {
-    Route,
-} from "react-router-dom";
-
-export default class Login extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            formData: {
-                username: "",
-                password: ""
-            },
-            successfulLogin: false,
-        }
-        this.userid = null;
-        this.handleChange = this.handleChange.bind(this);
-    }
-    handleSubmitClick = async e => {
+    const handleSubmit = async e => {
         e.preventDefault();
         try {
-            const body = { "email":this.state.formData.username, "password":this.state.formData.password };
+            const body = { "email":username, "password":password };
             const response = await fetch("http://localhost:5000/users/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json", "Accept": "application/json" },
@@ -32,36 +16,27 @@ export default class Login extends React.Component {
             });
             const user = await response.json();
             if(user){
-                this.userid=user.userid;
-                this.setState({successfulLogin:true});
+                setToken({"token": user.userid});
             }
         } catch (error) {
             
         }
     }
-    handleChange(event) {
-        let formData = Object.assign({}, this.state.formData);
-        formData[event.target.name] = event.target.value;
-        this.setState({formData});
-    }
-    render () {
-        return (
-            <div>
-                {!this.state.successfulLogin && 
-                    <form className="login" onSubmit={this.handleSubmitClick}>
-                        <label>Email
-                            <input name="username" type="text" value={ this.state.formData.username } onChange={ this.handleChange } />
-                        </label>
-                        <label>Password
-                            <input name="password" type="password" value={ this.state.formData.password } onChange={ this.handleChange } />
-                        </label>
-                        <button>Submit</button>
-                    </form>
-                }
-                {this.state.successfulLogin && 
-                    <HomeScreen userid={this.userid}/>
-                }
-            </div>
-        );
-    }
+    return (
+        <div>
+            <form className="login" onSubmit={handleSubmit}>
+                <label>Email
+                    <input name="username" type="text" value={ username } onChange={e => setUserName(e.target.value)} />
+                </label>
+                <label>Password
+                    <input name="password" type="password" value={ password } onChange={e => setPassword(e.target.value)} />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+Login.propTypes = { 
+    setToken: PropTypes.func.isRequired
 }
